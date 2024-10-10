06b26ee6f67e59b9b4062f0035f05df33bbc0b77
Firuz Chamarov <firuz1199@mail.ru>
2024-10-10 06:21:18 +0400

    style changes

--- lib/util/todo_title.dart
+++ lib/util/todo_title.dart
@@ -6,10 +6,14 @@ class TodoTitle extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.all(25.0),
       child: Container(
+        padding: EdgeInsets.all(24),
         child: Text('Make Tutorial'),
-        decoration: BoxDecoration(color: Colors.yellow),
+        decoration: BoxDecoration(
+          color: Colors.yellow,
+          borderRadius: BorderRadius.circular(12)
+          ),
       ),
     );
   }
