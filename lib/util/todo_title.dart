e2cc4e830881a76529a9b960b7b3f88f70f2910b
Firuz Chamarov <firuz1199@mail.ru>
2024-10-10 06:36:24 +0400

    creating todo tasks

--- lib/util/todo_title.dart
+++ lib/util/todo_title.dart
@@ -9,7 +9,17 @@ class TodoTitle extends StatelessWidget {
       padding: const EdgeInsets.all(25.0),
       child: Container(
         padding: EdgeInsets.all(24),
-        child: Text('Make Tutorial'),
+
+        child: Row(
+          children: [
+
+            //CheckBox
+            Checkbox(value: value, onChanged: onChanged),
+            
+            //Task name
+            Text('Make Tutorial'),
+          ],
+        ),
         decoration: BoxDecoration(
           color: Colors.yellow,
           borderRadius: BorderRadius.circular(12)
