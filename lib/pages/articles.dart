715925c37af9bc108a1a0c8f3b0e393fcfd4ae91
Firuz Chamarov <firuz1199@mail.ru>
2024-10-27 19:12:08 +0400

    changes

--- lib/pages/articles.dart
+++ lib/pages/articles.dart
@@ -5,6 +5,34 @@ class Articles extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "News Articles",
+            style: TextStyle(color: Colors.white),
+            ),
+            backgroundColor: Colors.blue,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.network(
+                'https://via.placeholder.com/400x200',
+                width: double.infinity,
+                height: 200,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(height: 8),
+              Text(
+                "Artic"
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
