42f9fce13e47cbc8f33eae1acc0f2d0194931b32
Firuz Chamarov <firuz1199@mail.ru>
2024-10-28 15:35:35 +0400

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,8 @@ class MyApp extends StatelessWidget {
       // home: Rowtest(),
       // home: Productcard(),
       // home: Articles(),
-      home: Blogpost(),
+      // home: Blogpost(),
+      
     );
   }
 }
